3a37089546a28c9503d6761a0d36ea1d9c70bf33
MSI\ANH THU <truongthianhthu2011@gmail.com>
2025-05-06 01:41:17 +0700

    fix: upload file task 3

--- pages/Image_Processing_Chapter3.py
+++ pages/Image_Processing_Chapter3.py
@@ -2,146 +2,167 @@ import sys
 from PIL import Image
 import cv2
 import numpy as np
+from datetime import datetime
 sys.path.append('../library')
 from library.bosung_streamlit.sidebar import *
 from library.bosung_streamlit.Chapter03 import *
 
-
 list_images_c3 = ['','1. Negative Image.png','2. Logarit Image.png','3. LuyThua.png','4. BienDoiTuyenTinhTungPhan.png','5. Histogram.png','6. Can Bang Histogram.png','7. Can Bang Histogram Anh Mau.png','8. Local Histogram.png','9. Thong Ke Histogram.png','10. Loc Box.png','11. Loc Gauss.png','12. PhanNguong.png','13. Loc_Median.png','14. Shapen.png','15. Gradient.png']
 
 def XuLyAnh():
-	global image_file
-	global c1,c2
-	st.title("Xử lý ảnh - "+Get_XyLyAnh_C3())
-	image_file = st.file_uploader("Upload Images", type=["bmp", "png","jpg","jpeg"])
-	c1,c2 = st.columns(2)
+    global image_file
+    global c1,c2
+    st.title("Xử lý ảnh - "+Get_XyLyAnh_C3())
+    image_file = st.file_uploader("Upload Images", type=["bmp", "png","jpg","jpeg"])
+    c1,c2 = st.columns(2)
+
 imgin = None
 def InsertImage(image_file,path=''):
-	global image
-	if image_file is not None:
-		image = Image.open(image_file) # You must transfome your picture into grey
-		c1.image(image, caption=None)
-		
-		# Chuyển sang cv2 để dùng sau này
-		frame = np.array(image)
-		frame = frame[:, :, [2, 1, 0]] # BGR -> RGB
-		image.close()
-	else:
-		image = Image.open("./images/ImageProcessing_Chapter3/"+path) # You must transfome your picture into grey
-		c1.image(image, caption=None)
-		image.close()
+    global image
+    if image_file is not None:
+        image = Image.open(image_file)
+        c1.image(image, caption=None)
+    else:
+        image = Image.open("./images/ImageProcessing_Chapter3/"+path)
+        c1.image(image, caption=None)
+        image.close()
 
 def Chosen_Proccesing(option,path=''):
-	global imgin
-	if(image_file is not None):
-		path=image_file.name
-	else:
-		InsertImage(image_file,list_images_c3[Get_Index_Image()])
-	imgin = cv2.imread("./images/ImageProcessing_Chapter3/"+path,cv2.IMREAD_GRAYSCALE)
+    global imgin
+    if image_file is not None:
+        # Mở lại file để xử lý
+        image = Image.open(image_file)
+        image_array = np.array(image)
+        # Chuyển sang grayscale nếu là ảnh màu
+        if len(image_array.shape) == 3:
+            if option == '7. Cân bằng Histogram của ảnh màu.':
+                imgin = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
+            else:
+                imgin = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+        else:
+            imgin = image_array
+        image.close()
+    else:
+        InsertImage(image_file,list_images_c3[Get_Index_Image()])
+        if option == '7. Cân bằng Histogram của ảnh màu.':
+            imgin = cv2.imread("./images/ImageProcessing_Chapter3/"+path, cv2.IMREAD_COLOR)
+        else:
+            imgin = cv2.imread("./images/ImageProcessing_Chapter3/"+path, cv2.IMREAD_GRAYSCALE)
 
-	if(option == '1. Negative Image.'):
-		Image_Negative()
-	elif(option == '2. Lograrit ảnh.'):
-		Image_Logarit()
-	elif(option == '3. Lũy thừa ảnh.'):
-		Image_Power()
-	elif(option == '4. Biến đổi tuyến tính từng phần.'):
-		Image_PiecewiseLinear()
-	elif(option == '5. Histogram'):
-		Image_Histogram()
-	elif(option == '6. Cân bằng Histogram'):
-		Image_HistEqual()
-	elif(option == '7. Cân bằng Histogram của ảnh màu.'):
-		image_temp = cv2.imread("./images/ImageProcessing_Chapter3/"+path,cv2.IMREAD_COLOR)
-		Image_HistEqualColor(image_temp)
-	elif(option == '8. Local Histogram.'):
-		Image_LocalHist()
-	elif(option == '9. Thống kê Histogram'):
-		Image_HistStat()	
-	elif(option == '10. Lọc Box'):
-		Image_MyBoxFilter()
-	elif(option == '11. Lọc Gauss'):
-		Image_onLowpassGauss()
-	elif(option == '12. Phân Ngưỡng'):
-		Image_Threshold()
-	elif(option == '13. Lọc Median'):
-		Image_MedianFilter()
-	elif(option == '14. Sharpen'):
-		Image_Sharpen()
-	elif(option == '15. Gradient'):
-		Image_Gradient()
-def Image_All():
-	global image_file
-	if(Get_XyLyAnh_C3()!=''):
-		if(image_file is not None):
-			# Mo de xu ly
-			InsertImage(image_file)
-			Chosen_Proccesing(Get_XyLyAnh_C3())
-		else:
-			Chosen_Proccesing(Get_XyLyAnh_C3(),list_images_c3[Get_Index_Image()])
+    if(option == '1. Negative Image.'):
+        Image_Negative()
+    elif(option == '2. Lograrit ảnh.'):
+        Image_Logarit()
+    elif(option == '3. Lũy thừa ảnh.'):
+        Image_Power()
+    elif(option == '4. Biến đổi tuyến tính từng phần.'):
+        Image_PiecewiseLinear()
+    elif(option == '5. Histogram'):
+        Image_Histogram()
+    elif(option == '6. Cân bằng Histogram'):
+        Image_HistEqual()
+    elif(option == '7. Cân bằng Histogram của ảnh màu.'):
+        Image_HistEqualColor(imgin)
+    elif(option == '8. Local Histogram.'):
+        Image_LocalHist()
+    elif(option == '9. Thống kê Histogram'):
+        Image_HistStat()    
+    elif(option == '10. Lọc Box'):
+        Image_MyBoxFilter()
+    elif(option == '11. Lọc Gauss'):
+        Image_onLowpassGauss()
+    elif(option == '12. Phân Ngưỡng'):
+        Image_Threshold()
+    elif(option == '13. Lọc Median'):
+        Image_MedianFilter()
+    elif(option == '14. Sharpen'):
+        Image_Sharpen()
+    elif(option == '15. Gradient'):
+        Image_Gradient()
 
+# Các hàm xử lý ảnh giữ nguyên
 def Image_Negative():
-	output_image = Negative(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Negative(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_Logarit():
-	output_image = Logarit(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Logarit(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_Power():
-	output_image = Power(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Power(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_PiecewiseLinear():
-	output_image = PiecewiseLinear(imgin)
-	c2.image(output_image, caption=None)
+    output_image = PiecewiseLinear(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_Histogram():
-	output_image = Histogram(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Histogram(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_HistEqual():
-	output_image = HistEqual(imgin)
-	c2.image(output_image, caption=None)
+    output_image = HistEqual(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_HistEqualColor(image_temp):
-	output_image = HistEqualColor(image_temp)
-	c2.image(output_image, caption=None)
+    output_image = HistEqualColor(image_temp)
+    c2.image(output_image, caption=None)
 
 def Image_LocalHist():
-	output_image = LocalHist(imgin)
-	c2.image(output_image, caption=None)
+    output_image = LocalHist(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_HistStat():
-	output_image = HistStat(imgin)
-	c2.image(output_image, caption=None)
+    output_image = HistStat(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_MyBoxFilter():
-	output_image = MyBoxFilter(imgin)
-	c2.image(output_image, caption=None)
+    output_image = MyBoxFilter(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_MedianFilter():
-	output_image = MedianFilter(imgin)
-	c2.image(output_image, caption=None)
+    output_image = MedianFilter(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_Threshold():
-	output_image = Threshold(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Threshold(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_Sharpen():
-	output_image = Sharpen(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Sharpen(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_Gradient():
-	output_image = Gradient(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Gradient(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_onLowpassGauss():
-	output_image = cv2.GaussianBlur(imgin,(43,43),7.0)
-	c2.image(output_image, caption=None)
+    output_image = cv2.GaussianBlur(imgin,(43,43),7.0)
+    c2.image(output_image, caption=None)
+
+def Image_All():
+    global image_file
+    if(Get_XyLyAnh_C3()!=''):
+        if(image_file is not None):
+            InsertImage(image_file)
+            Chosen_Proccesing(Get_XyLyAnh_C3())
+        else:
+            Chosen_Proccesing(Get_XyLyAnh_C3(),list_images_c3[Get_Index_Image()])
 
 if __name__=="__main__":
-	configure()
-	cs_sidebar()
-	XuLyAnh()
-	Image_All()
+    configure()
+    cs_sidebar()
+    XuLyAnh()
+    Image_All()
+    
+    # Footer với định dạng thời gian
+    current_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    st.markdown("---")
+    st.markdown(
+        f"""<div style='text-align: center'>
+        <small>Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): {current_time}</small><br>
+        <small>Current User's Login: anhthuu18</small>
+        </div>""",
+        unsafe_allow_html=True
+    )
--- pages/Image_Processing_Chapter4.py
+++ pages/Image_Processing_Chapter4.py
@@ -6,75 +6,81 @@ sys.path.append('../library')
 from library.bosung_streamlit.sidebar import *
 from library.bosung_streamlit.Chapter04 import *
 
-
+# Sửa lại list ảnh cho đúng với yêu cầu
 list_images_c4_0 = ['','1. Spectrum.png','2. Highpass filter.png','','3. Moire.png']
 
 def XuLyAnh():
-	global image_file
-	global c1,c2
-	st.title("Xử lý ảnh - "+Get_XyLyAnh_C4_0())
-	image_file = st.file_uploader("Upload Images", type=["bmp", "png","jpg","jpeg"])
-	c1,c2 = st.columns(2)
+    global image_file
+    global c1,c2
+    st.title("Xử lý ảnh - "+Get_XyLyAnh_C4_0())
+    image_file = st.file_uploader("Upload Images", type=["bmp", "png","jpg","jpeg"])
+    c1,c2 = st.columns(2)
+
 imgin = None
 def InsertImage(image_file,path=''):
-	global image
-	if image_file is not None:
-		image = Image.open(image_file) # You must transfome your picture into grey
-		c1.image(image, caption=None)
-		# Chuyển sang cv2 để dùng sau này
-		frame = np.array(image)
-		frame = frame[:, :, [2, 1, 0]] # BGR -> RGB
-		image.close()
-	else:
-		image = Image.open("./images/ImageProcessing_Chapter4/"+path) # You must transfome your picture into grey
-		c1.image(image, caption=None)
-		image.close()
+    global image
+    if image_file is not None:
+        image = Image.open(image_file)
+        c1.image(image, caption=None)
+    else:
+        image = Image.open("./images/ImageProcessing_Chapter4/"+path)
+        c1.image(image, caption=None)
+        image.close()
 
 def Chosen_Proccesing(option,path=''):
-	global imgin
-	if(option != '3. Vẽ bộ lọc note-reject'):
-		if(image_file is not None):
-			path=image_file.name
-		else:
-			InsertImage(image_file,list_images_c4_0[Get_Index_Image_C4_0()])
-		imgin = cv2.imread("./images/ImageProcessing_Chapter4/"+path,cv2.IMREAD_GRAYSCALE)
+    global imgin
+    if(option != '3. Vẽ bộ lọc note-reject'):  # Giữ nguyên điều kiện đặc biệt này
+        if image_file is not None:
+            # Mở lại file để xử lý
+            image = Image.open(image_file)
+            image_array = np.array(image)
+            # Chuyển sang grayscale nếu là ảnh màu
+            if len(image_array.shape) == 3:
+                imgin = cv2.cvtColor(image_array, cv2.COLOR_RGB2GRAY)
+            else:
+                imgin = image_array
+            image.close()
+        else:
+            InsertImage(image_file,list_images_c4_0[Get_Index_Image_C4_0()])
+            imgin = cv2.imread("./images/ImageProcessing_Chapter4/"+path, cv2.IMREAD_GRAYSCALE)
 
-	if(option == '1. Spectrum'):
-		Image_Spectrum()
-	elif(option == '2. Lọc trong miền tần số'):
-		Image_FrequencyFilter()
-	elif(option == '3. Vẽ bộ lọc note-reject'):
-		Image_CreateNotchRejectFilter()
-	elif(option == '4. Xóa nhiễu Moire'):
-		Image_RemoveMoire()
+    if(option == '1. Spectrum'):
+        Image_Spectrum()
+    elif(option == '2. Lọc trong miền tần số'):
+        Image_FrequencyFilter()
+    elif(option == '3. Vẽ bộ lọc note-reject'):
+        Image_CreateNotchRejectFilter()
+    elif(option == '4. Xóa nhiễu Moire'):
+        Image_RemoveMoire()
+
 def Image_All():
-	global image_file
-	if(Get_XyLyAnh_C4_0()!=''):
-		if(image_file is not None):
-			# Mo de xu ly
-			InsertImage(image_file)
-			#Chosen_Proccesing(Get_XyLyAnh_C4_0())
-		else:
-			Chosen_Proccesing(Get_XyLyAnh_C4_0(),list_images_c4_0[Get_Index_Image_C4_0()])
+    global image_file
+    if(Get_XyLyAnh_C4_0()!=''):
+        if(image_file is not None):
+            # Mở để xử lý
+            InsertImage(image_file)
+            Chosen_Proccesing(Get_XyLyAnh_C4_0())  # Bỏ comment dòng này
+        else:
+            Chosen_Proccesing(Get_XyLyAnh_C4_0(),list_images_c4_0[Get_Index_Image_C4_0()])
 
 def Image_Spectrum():
-	output_image = Spectrum(imgin)
-	c2.image(output_image, caption=None)
+    output_image = Spectrum(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_FrequencyFilter():
-	output_image = FrequencyFilter(imgin)
-	c2.image(output_image, caption=None)
+    output_image = FrequencyFilter(imgin)
+    c2.image(output_image, caption=None)
 
 def Image_CreateNotchRejectFilter():
-	output_image = CreateNotchRejectFilter()
-	c2.image(output_image, caption=None)
+    output_image = CreateNotchRejectFilter()
+    c2.image(output_image, caption=None)
 
 def Image_RemoveMoire():
-	output_image = RemoveMoire(imgin)
-	c2.image(output_image, caption=None)
+    output_image = RemoveMoire(imgin)
+    c2.image(output_image, caption=None)
 
 if __name__=="__main__":
-	configure()
-	cs_sidebar()
-	XuLyAnh()
-	Image_All()
+    configure()
+    cs_sidebar()
+    XuLyAnh()
+    Image_All()
